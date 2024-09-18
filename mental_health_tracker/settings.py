35287630eb40813c62dc0e53a63f75266b2bd7d3
Muhammad Jordan <jordan.aziz18@gmail.com>
2024-09-18 11:23:36 +0700

    add login

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0z^06aat5byaqoqp+4fv^#71x$qx@bih!#6#iov&sc2kd^0uc('
 
+PRODUCTION = os.getenv("PRODUCTION", False)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id","https://muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id", "http://muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
