cf0f3c2f376141b0bea83aabadb154d2d169f04e
Muhammad Jordan <jordan.aziz18@gmail.com>
2024-09-18 10:32:04 +0700

    new pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0z^06aat5byaqoqp+4fv^#71x$qx@bih!#6#iov&sc2kd^0uc(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id", "http://muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id","https://muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id", "http://muhammad-jordan31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
